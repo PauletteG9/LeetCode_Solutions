5741ba72904867364f5208a4dd9ef3fb369d6e95
Paulette <paulettegudapati@gmail.com>
2026-05-30 23:51:52 +0530

    Update day7.py

---
+++ day7.py
@@ -0,0 +1,7 @@
+def searchInsert(self, nums: list[int], target: int) -> int:
+        for i in range(len(nums)):
+            if nums[i] == target:
+                return i
+            if nums[i]>target:
+                return i
+        return len(nums)
