44ec7548848305ac827fd7bfd980b9e53161a340
Paulette <paulettegudapati@gmail.com>
2026-06-01 18:53:09 +0530

    Update day8.py

--- day8.py
+++ day8.py
@@ -40,7 +40,7 @@ def concatWithReverse(self, nums: list[int]) -> list[int]:
 """You are given a string array events. Initially, score = 0 and counter = 0. Each element in events is one of the following: "0", "1", "2", "3", "4", "6": Add that value to the total score, "W": Increase the counter by 1. No score is added, "WD": Add 1 to the total score, "NB": Add 1 to the total score.
 Process the array from left to right. Stop processing when either: All elements in events have been processed, or The counter becomes 10. Return an integer array [score, counter], where: score is the final total score, counter is the final counter value.
 Difficulty: Easy
-Approach: Brute force"""
+Approach: Brute Force"""
 
 def scoreValidator(self, events: list[str]) -> list[int]:
         score, counter = 0, 0
