cd6bb6c9aa3bbcdef1b6ac13647b6c7d38470745
Paulette <paulettegudapati@gmail.com>
2026-06-07 00:42:01 +0530

    Update day10.py

---
+++ day10.py
@@ -0,0 +1,19 @@
+# 2574. Left and Right Sum Differences
+"""You are given a 0-indexed integer array nums of size n. Define two arrays leftSum and rightSum where:
+leftSum[i] is the sum of elements to the left of the index i in the array nums. If there is no such element, leftSum[i] = 0.
+rightSum[i] is the sum of elements to the right of the index i in the array nums. If there is no such element, rightSum[i] = 0.
+Return an integer array answer of size n where answer[i] = |leftSum[i] - rightSum[i]|.
+Difficulty: Easy
+Approach: Brute Force"""
+
+def leftRightDifference(self, nums: list[int]) -> list[int]:
+        left = 0
+        right = sum(nums)
+        leftSum = []
+        rightSum = []
+        for i in range(len(nums)):
+            leftSum.append(left)
+            left += nums[i]
+            right -= nums[i]
+            rightSum.append(right)
+        return [abs(leftSum[i] - rightSum[i]) for i in range(len(nums))]
