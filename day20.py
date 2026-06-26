3e3bf217cc34d49f783f155812c1df5f6272e286
Paulette <paulettegudapati@gmail.com>
2026-06-26 16:33:39 +0530

    Update day20.py

--- day20.py
+++ day20.py
@@ -5,10 +5,27 @@ Approach: Hash set, just find num that occurs twice, since all other nums occur 
 TC : O(n)
 SC : O(n)"""
  
-
 def repeatedNTimes(self, nums: list[int]) -> int:
     freq = {}
     for num in nums:
         freq[num] = freq.get(num, 0) +1
         if freq[num] > 1:
             return num
+
+# 3866. First Unique Even Element
+"""You are given an integer array nums. Return an integer denoting the first even integer (earliest by array index) that appears exactly once in nums. If no such integer exists, return -1.
+Difficulty: Easy
+Approach: Hash set.
+TC : O(n)
+SC : O(n)"""
+
+def firstUniqueEven(self, nums: list[int]) -> int:
+        freq = {}
+        for num in nums:
+            freq[num] = freq.get(num, 0)+1
+        
+        for num, count in freq.items():
+            if count == 1 and num%2 == 0:
+                return num
+            
+        return -1
